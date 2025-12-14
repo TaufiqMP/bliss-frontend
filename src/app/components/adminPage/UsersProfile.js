53eacc8b26f7e4cba1df4c577cc0dc8507d9902c
naufangit <naufan.fardan@gmail.com>
2025-12-14 19:04:19 +0700

    delete banner

--- src/app/components/adminPage/UsersProfile.js
+++ src/app/components/adminPage/UsersProfile.js
@@ -4,27 +4,28 @@ import Image from "next/image";
 
 export default function UserProfile({ imageProfile, onImageChange }) {
     return (
-        <div className="relative w-full h-60 overflow-visible shadow-md">
-            <Image
-                src="/images/background.png"
-                alt="background"
-                fill
-                className="object-contain p-2"
-                loading="eager"
-            />
+        <div className="relative w-full h-32 overflow-visible">
+
+            {/* Profile Picture */}
             <div className="absolute -bottom-14 left-10">
                 <div className="relative w-32 h-32 rounded-full overflow-hidden border-4 border-white shadow-lg group">
+
                     <Image
-                        src={imageProfile || `https://cmldqhfeingvkdeurufd.supabase.co/storage/v1/object/public/avatar/default.jpg`}
+                        src={
+                            imageProfile ||
+                            "https://cmldqhfeingvkdeurufd.supabase.co/storage/v1/object/public/avatar/default.jpg"
+                        }
                         alt="profile"
                         fill
                         className="object-cover"
                         sizes="(max-width: 768px) 100vw, (max-width: 800px) 50vw, 33vw"
                     />
+
+                    {/* Hover overlay */}
                     <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition flex items-center justify-center">
                         <label
                             htmlFor="profileUpload"
-                            className="cursor-pointer text-white text-sm px-3 py-1"
+                            className="cursor-pointer text-white"
                         >
                             <MdOutlineMode className="text-xl" />
                         </label>
@@ -36,8 +37,9 @@ export default function UserProfile({ imageProfile, onImageChange }) {
                             onChange={onImageChange}
                         />
                     </div>
+
                 </div>
             </div>
         </div>
-    )
+    );
 }
