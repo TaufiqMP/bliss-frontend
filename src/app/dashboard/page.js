3def8d2ec25508f597f2002d65b10dfc24df48da
Nexarion <fendipermadi058@gmail.com>
2025-12-14 21:08:56 +0700

    add all nasabah when admin logged in3

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -15,7 +15,7 @@ export default async function DashboardServer() {
   const userData = await getUserData(userId, token);
 
   let data;
-  if (userData?.data?.role === 'admin') {
+  if (userData?.data?.role_id === 1) {
     data = await getNasabah();
   } else {
     data = await getNasabahSpecific(userId);
