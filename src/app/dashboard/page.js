259cb0c5243643c1480cd6c787c35775b18f4eae
Taufiq <taufiqmaulanapr@gmail.com>
2025-12-14 22:26:48 +0700

    fix admin 3

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -35,7 +35,7 @@ export default async function DashboardServer() {
 
   return (
     <>
-      <DashboardClient data={data.data} token={token} userId={userId} topThree={topthree} openClosed={openClosed} user={userData.data} />
+      <DashboardClient data={data.data} token={token} userId={userId} topThree={topthree} openClosed={count} user={userData.data} />
     </>
   )
 }
