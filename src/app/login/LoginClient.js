cc8aad67ba2e40497a386bb10f751b08ef57d665
Taufiq <taufiqmaulanapr@gmail.com>
2025-12-14 14:34:06 +0700

    Fix caoksaokcosak

--- src/app/login/LoginClient.js
+++ src/app/login/LoginClient.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState(""); 
-  
+   const [isLoading, setIsLoading] = useState(false);
 
   const togglePassword = () => setShowPassword(!showPassword);
 
