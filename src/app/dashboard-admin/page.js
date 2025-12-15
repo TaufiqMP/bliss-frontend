bd3d992a84e163d178fac24a11978f3966302e2a
Taufiq <taufiqmaulanapr@gmail.com>
2025-12-15 13:39:56 +0700

    testing 6

--- src/app/dashboard-admin/page.js
+++ src/app/dashboard-admin/page.js
@@ -1,10 +1,14 @@
-import DashboardAdminClient from "./AdminClient"
+import DashboardAdminClient from "./DashboardAdminClient"
 import { getAccessToken } from "@/utils/cookies";
 import { decodeAccessToken } from "@/utils/jwt";
 import { getUserData } from "@/utils/api";
 
-export default async function() {
+
+export const dynamic = 'force-dynamic';
+
+export default async function ServerAdmin() {
   const token = await getAccessToken();
+
   const userId = await decodeAccessToken();
 
   const userData = await getUserData(userId, token);
